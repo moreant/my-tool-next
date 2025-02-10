b2c3f547d250c49e0fc38b6a614e8019f54da9b1
molingran <molingran@outlook.com>
2025-02-10 17:19:52 +0800

    feat(convert): 增强文本去除空白功能，支持更多去除模式

--- src/views/text-convert/trim/index.vue
+++ src/views/text-convert/trim/index.vue
@@ -5,12 +5,22 @@ import TextInputFunc from '@/components/common/text-input-func.vue';
 const value = ref('');
 const type = ref('both');
 
-watch(value, () => {
+const format = () => {
   if (type.value === 'both') {
     value.value = value.value.trim();
+  } else if (type.value === 'spaceAndTab') {
+    value.value = value.value.replace(/[ \t]/g, '');
   } else if (type.value === 'all') {
     value.value = value.value.replace(/\s+/g, '');
   }
+};
+
+watch(value, () => {
+  format();
+});
+
+watch(type, () => {
+  format();
 });
 </script>
 
@@ -20,6 +30,7 @@ watch(value, () => {
       <NFlex vertical>
         <NRadioGroup v-model:value="type" name="type">
           <NRadio value="both">前后</NRadio>
+          <NRadio value="spaceAndTab">空格和制表符</NRadio>
           <NRadio value="all">全部</NRadio>
         </NRadioGroup>
         <TextInputFunc v-model:value="value" placeholder="请输入要处理的内容" />
